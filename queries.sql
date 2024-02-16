bf4edf83e27ec5d875f0cee9b00d6a60c4b93bd8
Austin Winn <awinn0711@gmail.com>
2024-02-15 18:28:17 -0600

    passing all tests locally, application working

--- queries.sql
+++ queries.sql
@@ -10,3 +10,7 @@
 --Part 3
     DROP TABLE job;
 --Part 4
+    SELECT * FROM skill
+    LEFT JOIN job_skills on skill.id = job_skills.skills_id
+    WHERE job_skills.jobs_id IS NOT NULL
+    ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.persistent.controllers;
 import jakarta.validation.Valid;
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
 import org.launchcode.techjobs.persistent.models.data.SkillRepository;
@@ -43,19 +44,24 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
 	    model.addAttribute("title", "Add Job");
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId) {
+                                    Errors errors, Model model,
+                                    @RequestParam int employerId,
+                                    @RequestParam List<Integer> skills) {
 
         if (errors.hasErrors()) {
 	    model.addAttribute("title", "Add Job");
             return "add";
         }
         Optional<Employer> optionalEmployer = employerRepository.findById(employerId);
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        newJob.setSkills(skillObjs);
         if(optionalEmployer.isPresent()) {
             Employer employer = (Employer) optionalEmployer.get();
             newJob.setEmployer(employer);
